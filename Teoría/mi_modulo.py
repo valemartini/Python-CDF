27d8581f5d3927ad7ab26c9b5e669e4960f11d8c
unknown <valemartini@gmail.com>
2023-11-21 12:53:08 -0300

    Teoria

---
+++ Teoría/mi_modulo.py
@@ -0,0 +1,11 @@
+def saludar():
+    print("Hola, soy una función en mi_modulo.py")
+
+VELOCIDAD_LUZ = 299792458
+
+print("Codigo ejecutado desde mi_modulo.py")
+saludar()
+
+# if __name__ == '__main__':
+#     print("Codigo ejecutado desde mi_modulo.py")
+#     saludar()
---
+++ Teoría/principal.py
@@ -0,0 +1,6 @@
+import mi_modulo
+
+if __name__ == '__main__':
+
+    print("Codigo ejecutado desde principal.py")
+    mi_modulo.saludar()
